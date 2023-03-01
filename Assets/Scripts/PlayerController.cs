b8cf85db4145543556cafffbb605cd243805c14b
alwong18 <121971634+alwong18@users.noreply.github.com>
2023-03-01 11:16:10 -0800

    Fixed Mistake
    Debug Log Works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollisonEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
